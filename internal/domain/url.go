8450de0dc6aad0f014492280571495f05e42d469
MeBr0 <mebr0mebr1@gmail.com>
2021-07-17 01:22:55 +0600

    fix misspell

--- internal/domain/url.go
+++ internal/domain/url.go
@@ -16,7 +16,7 @@ type URL struct {
 	// Expiration time
 	ExpiredAt time.Time `json:"expiredAt" bson:"expiredAt" format:"yyyy-MM-ddThh:mm:ss.ZZZ" example:"2021-06-09T09:29:18.169Z"`
 	// Id of owner
-	Owner primitive.ObjectID `json:"owner" bson:"owner" format:"hexidecimal string" example:"6095872d75ff40c9238bdb29"`
+	Owner primitive.ObjectID `json:"owner" bson:"owner" format:"hexadecimal string" example:"6095872d75ff40c9238bdb29"`
 } // @name URL
 
 type URLCreate struct {
